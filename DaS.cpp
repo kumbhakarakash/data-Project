f2c8f25d0d2d5b1427dac3395f8c15c13e31b3a6
akash kumbhakar <62172814+kumbhakarakash@users.noreply.github.com>
2023-03-25 20:18:49 +0530

    Add files via upload

---
+++ DaS.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<vector>
+
+using namespace std;
+struct record{
+    string studentID;
+    string name;
+    char gender;
+    short int day;
+    string month;
+    string homecity;
+    float math;
+    float phy;
+    float chem;
+    float total;
+};
+
+int main(){
+    vector<record>student_record;
+    ifstream student_score_file("score.CSV");
+    if(!student_score_file.is_open()){
+        cerr<<"Error!! Openning file!!"<<endl;
+        return 1;
+    }
+    string line;
+    while(getline(student_score_file, line)){
+        static bool skip = true;
+        if(skip){
+            skip = false;
+            continue;
+        }
+        stringstream ss(line);
+        record student;
+        string word;
+        while(getline(ss,word,',')){
+            cout<<word<<" ";
+        }
+        cout<<endl;
+
+    }
+    student_score_file.close();
+
+    return 0;
+}
