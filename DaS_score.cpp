c42d0e843fd83ee764a0590e25fe02aae48f15c3
akash kumbhakar <62172814+kumbhakarakash@users.noreply.github.com>
2023-04-19 14:39:02 +0530

    Update DaS_score.cpp
    modularization of data extraction process in  a different function ...

--- DaS_score.cpp
+++ DaS_score.cpp
@@ -3,12 +3,13 @@
 #include<sstream>
 #include<string>
 #include<vector>
+#include<map>
 
 using namespace std;
 struct record{
     string studentID;
     string name;
-    string gender;
+    char gender;
     short int day;
     string month;
     string homecity;
@@ -17,11 +18,9 @@ struct record{
     float chem;
     float total;
 };
-
-int main(){
-    vector<record>student_record;                               //a vector of struct type that will keep the value from score.CSV file
-    ifstream student_score_file("score.CSV");                   //where each student is represented by a single structure data type
-    if(!student_score_file.is_open()){                          //here we name it as---- record
+bool extractData(string source_file,vector<record> &student_record){//funtion that will extract data from source file to a vector...
+    ifstream student_score_file(source_file);
+    if(!student_score_file.is_open()){
         cerr<<"Error!! Openning file!!"<<endl;
         return 1;
     }
@@ -39,10 +38,10 @@ int main(){
             con_word.push_back(word);
         }
         record student;
-        //here we are populating structure datatype--student......
+        //these are updating structure from the data from con_word
         student.studentID = con_word[0];
         student.name = con_word[1];
-        student.gender = con_word[2];
+        student.gender = con_word[2].c_str()[0];
         student.day = stoi(con_word[3]);
         student.month = con_word[4];
         student.homecity = con_word[5];
@@ -50,13 +49,29 @@ int main(){
         student.phy = stof(con_word[7]);
         student.chem = stof(con_word[8]);
         student.total = stof(con_word[9]);
-        //this create a vector of records taking each word from each line..... 
+        //this will store individual record into vector student_record
         student_record.push_back(student);
 
     }
-    //here we accessing data from the vector... 
-    cout<<student_record[29].math;
-    student_score_file.close();
+    return 0;
+}
 
+int main(){
+    vector<record>student_record;                      
+    extractData("score.csv", student_record);
+    //at this level we have our data inside vector named student_record....
+    //now we can utilize those data to extract different information....
+    int Mcount = 0,Fcount = 0,Tcount = 0;
+    for(int i = 0; i < student_record.size(); i++){
+        if(student_record[i].gender == 'M'){
+            Mcount = Mcount + 1;
+            Tcount = Tcount + 1;
+        }
+        else{
+            Fcount = Fcount + 1;
+            Tcount = Tcount + 1;
+        }
+    }
+    cout<<"MaleStudent="<<Mcount<<"\n"<<"FemaleStudent="<<Fcount<<"\n"<<"TotalStudent="<<Tcount<<"\n";
     return 0;
 }
